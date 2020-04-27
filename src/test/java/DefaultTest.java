324941eb0e9764798a82aaa576c31caa6c155073
Andrin Bertschi <abertschi@abertschi.ch>
2020-04-27 12:50:57 +0200

    init fatjar

---
+++ src/test/java/DefaultTest.java
@@ -0,0 +1,23 @@
+//import org.eclipse.php.core.PHPVersion;
+//import org.eclipse.php.core.ast.nodes.ASTNode;
+//import org.eclipse.php.core.ast.nodes.ASTParser;
+//import org.eclipse.php.core.ast.nodes.Program;
+//import org.eclipse.php.core.ast.visitor.HierarchicalVisitor;
+
+import org.eclipse.php.core.PHPVersion;
+import org.eclipse.php.core.ast.nodes.ASTParser;
+import org.eclipse.php.core.ast.nodes.Program;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class DefaultTest {
+
+
+    @Test
+    public void test() throws Exception {
+
+    }
+}
